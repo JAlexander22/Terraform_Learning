c8ecbf5d7c8d9a3ee9793e641775f7e6f82ec092
JAlexander22 <jordan-alexander99@hotmail.co.uk>
2025-01-28 21:27:54 +0000

    Added User Data to Terrform

--- main.tf
+++ main.tf
@@ -21,7 +21,18 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = "JordansAppServer"
   }
+  user_data = file("script.sh")
+  # user_data = <<-EOT
+  #   #!/bin/bash
+  #   sudo yum update -y
+  #   sudo yum install -y curl wget git vim htop
+  #   sudo yum install -y httpd
+  #   sudo systemctl enable httpd
+  #   sudo systemctl start httpd
+  # EOT
 }
+
+
 
 output "instance_instance_state" {
   description = "Public IP Address of the EC2 instance"
